2fc121d892504f768b4befa41377e8699f888db0
Jérémy Zurcher <jeremy@asynk.ch>
2011-04-09 00:13:56 +0200

    update efl.rb

--- lib/efl.rb
+++ lib/efl.rb
@@ -3,7 +3,10 @@
 #
 #
 module EFL
-    autoload :EET, './lib/efl/eet.rb'
+    autoload :EINA,  './lib/efl/eina.rb'
+    autoload :EET,   './lib/efl/eet.rb'
+    autoload :EVAS,  './lib/efl/evas.rb'
     autoload :ECORE, './lib/efl/ecore.rb'
+    autoload :EDJE,  './lib/efl/edje.rb'
 end
 #
