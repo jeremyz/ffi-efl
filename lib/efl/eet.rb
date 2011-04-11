455dd7ecbbe137fef1ff5507055541350a924382
Jérémy Zurcher <jeremy@asynk.ch>
2011-04-11 22:38:20 +0200

    move EET::Error to file bottom

--- lib/efl/eet.rb
+++ lib/efl/eet.rb
@@ -6,51 +6,6 @@ require 'ffi'
 module EFL
     module EET
         #
-        class Error < Exception
-            ERROR_NONE=0
-            ERROR_BAD_OBJECT=1
-            ERROR_EMPTY=2
-            ERROR_NOT_WRITABLE=3
-            ERROR_OUT_OF_MEMORY=4
-            ERROR_WRITE_ERROR=5
-            ERROR_WRITE_ERROR_FILE_TOO_BIG=6
-            ERROR_WRITE_ERROR_IO_ERROR=7
-            ERROR_WRITE_ERROR_OUT_OF_SPACE=8
-            ERROR_WRITE_ERROR_FILE_CLOSED=9
-            ERROR_MMAP_FAILED=10
-            ERROR_X509_ENCODING_FAILED=11
-            ERROR_SIGNATURE_FAILED=12
-            ERROR_INVALID_SIGNATURE=13
-            ERROR_NOT_SIGNED=14
-            ERROR_NOT_IMPLEMENTED=15
-            ERROR_PRNG_NOT_SEEDED=16
-            ERROR_ENCRYPT_FAILED=17
-            ERROR_DECRYPT_FAILED=18
-            #
-            MSGS = {
-                0=>'No error, it\'s all fine!',
-                1=>' Given object or handle is NULL or invalid',
-                2=>'There was nothing to do',
-                3=>'Could not write to file or fine is #FILE_MODE_READ',
-                4=>'Could not allocate memory',
-                5=>'Failed to write data to destination',
-                6=>'Failed to write file since it is too big',
-                7=>'Failed to write since generic Input/Output error',
-                8=>'Failed to write due out of space',
-                9=>'Failed to write because file was closed',
-                10=>'Could not mmap file',
-                11=>'Could not encode using X509',
-                12=>'Could not validate signature',
-                13=>'Signature is invalid',
-                14=>'File or contents are not signed',
-                15=>'Function is not implemented',
-                16=>'Could not introduce random seed',
-                17=>'Could not encrypt contents',
-                18=>'Could not decrypt contents',
-            }
-            #
-        end
-        #
         extend FFI::Library
         #
         ffi_lib 'eet'
@@ -123,6 +78,51 @@ module EFL
                 return nil if s==0
                 ( data.null? ? nil : data.read_string[0..s-1] )
             end
+            #
+        end
+        #
+        class Error < Exception
+            ERROR_NONE=0
+            ERROR_BAD_OBJECT=1
+            ERROR_EMPTY=2
+            ERROR_NOT_WRITABLE=3
+            ERROR_OUT_OF_MEMORY=4
+            ERROR_WRITE_ERROR=5
+            ERROR_WRITE_ERROR_FILE_TOO_BIG=6
+            ERROR_WRITE_ERROR_IO_ERROR=7
+            ERROR_WRITE_ERROR_OUT_OF_SPACE=8
+            ERROR_WRITE_ERROR_FILE_CLOSED=9
+            ERROR_MMAP_FAILED=10
+            ERROR_X509_ENCODING_FAILED=11
+            ERROR_SIGNATURE_FAILED=12
+            ERROR_INVALID_SIGNATURE=13
+            ERROR_NOT_SIGNED=14
+            ERROR_NOT_IMPLEMENTED=15
+            ERROR_PRNG_NOT_SEEDED=16
+            ERROR_ENCRYPT_FAILED=17
+            ERROR_DECRYPT_FAILED=18
+            #
+            MSGS = {
+                0=>'No error, it\'s all fine!',
+                1=>' Given object or handle is NULL or invalid',
+                2=>'There was nothing to do',
+                3=>'Could not write to file or fine is #FILE_MODE_READ',
+                4=>'Could not allocate memory',
+                5=>'Failed to write data to destination',
+                6=>'Failed to write file since it is too big',
+                7=>'Failed to write since generic Input/Output error',
+                8=>'Failed to write due out of space',
+                9=>'Failed to write because file was closed',
+                10=>'Could not mmap file',
+                11=>'Could not encode using X509',
+                12=>'Could not validate signature',
+                13=>'Signature is invalid',
+                14=>'File or contents are not signed',
+                15=>'Function is not implemented',
+                16=>'Could not introduce random seed',
+                17=>'Could not encrypt contents',
+                18=>'Could not decrypt contents',
+            }
             #
         end
     end
