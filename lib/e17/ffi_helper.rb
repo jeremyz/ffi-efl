794e67ddb56539fcd94ab2b29fbd2894b53f9066
Jérémy Zurcher <jeremy.zurcher@heraeus.com>
2011-04-19 10:46:20 +0200

    ffi_helper: add needed default typedef

--- lib/e17/ffi_helper.rb
+++ lib/e17/ffi_helper.rb
@@ -4,6 +4,7 @@
 require 'ffi'
 #
 module FFIHelper
+    #
     def attach_fcts fcts
         fcts.each do |func|
             begin
@@ -13,12 +14,25 @@ module FFIHelper
             end
         end
     end
+    #
     def create_aliases l, fcts
         module_eval <<-code
         class << self
             #{fcts.map{ |f,a,r| "alias #{f.to_s[l..-1]} #{f}" }.join "\n" }
         end
         code
+    end
+    #
+    def self.extended mod
+        mod.module_eval <<-EOF
+        typedef :pointer, :int_p
+        typedef :pointer, :uint_p
+        typedef :pointer, :void_p
+        typedef :pointer, :uchar_p
+        typedef :pointer, :string_array
+        #
+        typedef :pointer, :eina_list_p
+        EOF
     end
 end
 #
