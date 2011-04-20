86a7573442727603b8fce7251dae811ad714b266
Jérémy Zurcher <jeremy@asynk.ch>
2011-04-20 19:46:58 +0200

    add efl.rb

---
+++ lib/efl.rb
@@ -0,0 +1,14 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+#
+module Efl
+#    autoload :Eina,  './lib/efl/eina.rb'
+    autoload :Eet,   './lib/efl/eet.rb'
+    autoload :Evas,  './lib/efl/evas.rb'
+    autoload :Ecore, './lib/efl/ecore.rb'
+    autoload :Edje,  './lib/efl/edje.rb'
+    autoload :Elm,   './lib/efl/elementary.rb'
+end
+#
+# EOF
