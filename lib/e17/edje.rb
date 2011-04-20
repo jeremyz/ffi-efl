1cc2c83a0241ff8d36811d40062ba26ae8e28485
Jérémy Zurcher <jeremy.zurcher@heraeus.com>
2011-04-20 09:47:47 +0200

    set edje lib and specs, remove eina lib and specs

--- lib/e17/edje.rb
+++ lib/e17/edje.rb
@@ -1,26 +1,10 @@
 #! /usr/bin/env ruby
 # -*- coding: UTF-8 -*-
 #
-require 'e17/ffi_helper'
+require 'e17/edje/edje-ffi'
 #
 module E17
-    module EDJE
-        #
-        extend FFIHelper
-        extend FFI::Library
-        #
-        ffi_lib 'edje'
-        #
-        fcts = [
-            # TODO
-            # http://docs.enlightenment.org/auto/edje/Edje_8h.html
-            [ :edje_init, [], :int],
-            [ :edje_shutdown, [], :int],
-        ]
-        #
-        attach_fcts fcts
-        #
-        create_aliases 'edje_'.length, fcts
+    module Edje
         #
     end
 end
--- spec/edje_spec.rb
+++ spec/edje_spec.rb
@@ -3,20 +3,20 @@
 #
 require 'e17/edje'
 #
-describe E17::EDJE do
+describe E17::Edje do
     #
     include E17
     #
     it "should init" do
-        EDJE.init.should eql 1
-        EDJE.init.should eql 2
-        EDJE.init.should eql 3
+        Edje.init.should eql 1
+        Edje.init.should eql 2
+        Edje.init.should eql 3
     end
     #
     it "should shutdown" do
-        EDJE.shutdown.should eql 2
-        EDJE.shutdown.should eql 1
-        EDJE.shutdown.should eql 0
+        Edje.shutdown.should eql 2
+        Edje.shutdown.should eql 1
+        Edje.shutdown.should eql 0
     end
     #
 end
