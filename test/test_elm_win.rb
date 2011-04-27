148dfc4de699f1619ba24b3fe02c55d27a35abd0
Jérémy Zurcher <jeremy@asynk.ch>
2011-04-27 16:06:09 +0200

    fis test_elm_win

--- test/test_elm_win.rb
+++ test/test_elm_win.rb
@@ -3,36 +3,34 @@
 #
 require 'efl/elementary'
 #
-include Efl
-#
-puts Elm.init
+Efl::Elm.init
 
 win_del = Proc.new { |data,evas_object,event_info|
-    Efl::API.elm_exit();
+    Efl::FFI.elm_exit();
 }
 
-win = Efl::API.elm_win_add FFI::MemoryPointer::NULL, "App name", :elm_win_basic
-Efl::API.elm_win_title_set win, "Window title"
-Efl::API.evas_object_smart_callback_add win, "delete,request", win_del, FFI::MemoryPointer::NULL
+win = Efl::FFI.elm_win_add ::FFI::MemoryPointer::NULL, "App name", :elm_win_basic
+Efl::FFI.elm_win_title_set win, "Window title"
+Efl::FFI.evas_object_smart_callback_add win, "delete,request", win_del, ::FFI::MemoryPointer::NULL
 
-bg = Efl::API.elm_bg_add win
-Efl::API.evas_object_size_hint_weight_set bg, 1.0, 1.0
-Efl::API.elm_win_resize_object_add win, bg
-Efl::API.evas_object_show bg
+bg = Efl::FFI.elm_bg_add win
+Efl::FFI.evas_object_size_hint_weight_set bg, 1.0, 1.0
+Efl::FFI.elm_win_resize_object_add win, bg
+Efl::FFI.evas_object_show bg
 
-lb = Efl::API.elm_label_add win
-Efl::API.elm_label_label_set lb, "Hello World!"
-Efl::API.evas_object_size_hint_weight_set lb, 1.0, 1.0
-Efl::API.elm_win_resize_object_add win, lb
-Efl::API.evas_object_show lb
+lb = Efl::FFI.elm_label_add win
+Efl::FFI.elm_label_label_set lb, "Hello World!"
+Efl::FFI.evas_object_size_hint_weight_set lb, 1.0, 1.0
+Efl::FFI.elm_win_resize_object_add win, lb
+Efl::FFI.evas_object_show lb
 
-Efl::API.evas_object_move win, 300, 300
-Efl::API.evas_object_resize win, 200, 100
+Efl::FFI.evas_object_move win, 300, 300
+Efl::FFI.evas_object_resize win, 200, 100
 
-Efl::API.evas_object_show win
+Efl::FFI.evas_object_show win
 
-Elm.run
-puts Elm.shutdown
+Efl::Elm.run
+Efl::Elm.shutdown
 #
 # EOF
 
