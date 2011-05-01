ecbb84df1bf1cfdb686dd34a302b584e77e739c4
Jérémy Zurcher <jeremy@asynk.ch>
2011-05-01 09:32:32 +0200

    update lib/efl, README, Changelog

--- lib/efl.rb
+++ lib/efl.rb
@@ -4,14 +4,19 @@
 #
 module Efl
     #
-    VERSION = '0.0.2'
+    VERSION = '0.0.3'
     #
-    autoload :Eet,          './lib/efl/eet.rb'
-    autoload :Evas,         './lib/efl/evas.rb'
-    autoload :Ecore,        './lib/efl/ecore.rb'
+    autoload :EcoreEvas,    './lib/efl/ecore_evas.rb'
     autoload :EcoreGetopt,  './lib/efl/ecore_getopt.rb'
+    autoload :EcoreInput,   './lib/efl/ecore_input.rb'
+    autoload :Ecore,        './lib/efl/ecore.rb'
     autoload :Edje,         './lib/efl/edje.rb'
+    autoload :Eet,          './lib/efl/eet.rb'
+    autoload :EinHash,      './lib/efl/eina_hash.rb'
+    autoload :EinaList,     './lib/efl/eina_list.rb'
+    autoload :Eina,         './lib/efl/eina.rb'
     autoload :Elm,          './lib/efl/elementary.rb'
+    autoload :Evas,         './lib/efl/evas.rb'
 end
 #
 # EOF
