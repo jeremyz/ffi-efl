138a131c1aea290f0154a5b6fcf466e6d8161ec8
Jérémy Zurcher <jeremy@asynk.ch>
2011-05-05 14:12:19 +0200

    update Changelog and VERSION

--- lib/efl.rb
+++ lib/efl.rb
@@ -4,7 +4,7 @@
 #
 module Efl
     #
-    VERSION = '0.0.3'
+    VERSION = '0.0.4'
     #
     autoload :EcoreEvas,    './lib/efl/ecore_evas.rb'
     autoload :EcoreGetopt,  './lib/efl/ecore_getopt.rb'
