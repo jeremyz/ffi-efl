2aa685afae252676850dd7721b2540258375593a
Jérémy Zurcher <jeremy@asynk.ch>
2011-05-07 00:54:04 +0200

    extract realize_evas from specs

--- spec/edje_spec.rb
+++ spec/edje_spec.rb
@@ -19,27 +19,9 @@ end
 require 'efl/eina_list'
 require 'efl/evas'
 require 'efl/edje'
+require './spec/helper'
 #
 describe Efl::Edje do
-    #
-    def realize_evas
-        width = 300
-        height = 200
-        @pixels = FFI::MemoryPointer.new :int, width*height
-        @e = Efl::Evas::REvas.new
-        @e.output_method_set Efl::Evas::render_method_lookup("buffer")
-        @e.output_viewport_set 0, 0, width, height
-        @e.output_size_set width, height
-        einfo = Efl::Native::EngineInfoBufferStruct.new @e.engine_info
-        einfo[:info][:depth_type] = Efl::Evas::EVAS_ENGINE_BUFFER_DEPTH_ARGB32
-        einfo[:info][:dest_buffer] = @pixels
-        einfo[:info][:dest_buffer_row_bytes] = width * FFI::type_size(:int);
-        einfo[:info][:use_color_key] = 0;
-        einfo[:info][:alpha_threshold] = 0;
-        einfo[:info][:func][:new_update_region] = nil #FFI::Pointer::NULL;
-        einfo[:info][:func][:free_update_region] = nil #FFI::Pointer::NULL;
-        @e.engine_info_set einfo
-    end
     #
     before(:all) { Edje = Efl::Edje }
     #
--- spec/evas_spec.rb
+++ spec/evas_spec.rb
@@ -4,27 +4,9 @@
 require 'efl/eina_list'
 require 'efl/ecore'
 require 'efl/evas'
+require './spec/helper'
 #
 describe Efl::Evas do
-    #
-    def realize_evas
-        width = 800
-        height = 600
-        @pixels = FFI::MemoryPointer.new :int, width*height
-        @e = Evas::REvas.new
-        @e.output_method_set Evas::render_method_lookup("buffer")
-        @e.output_viewport_set 0, 0, width, height
-        @e.output_size_set width, height
-        einfo = Native::EngineInfoBufferStruct.new @e.engine_info
-        einfo[:info][:depth_type] = Evas::EVAS_ENGINE_BUFFER_DEPTH_ARGB32
-        einfo[:info][:dest_buffer] = @pixels
-        einfo[:info][:dest_buffer_row_bytes] = width * FFI::type_size(:int);
-        einfo[:info][:use_color_key] = 0;
-        einfo[:info][:alpha_threshold] = 0;
-        einfo[:info][:func][:new_update_region] = nil #FFI::Pointer::NULL;
-        einfo[:info][:func][:free_update_region] = nil #FFI::Pointer::NULL;
-        @e.engine_info_set einfo
-    end
     #
     before(:all) {
         Evas = Efl::Evas
