c18b9432eeabad028cd252d6a983c5abf06d9ac2
Jérémy Zurcher <jeremy@asynk.ch>
2011-05-11 12:06:02 +0200

    update Changelog , REVISION, autoload

--- lib/efl.rb
+++ lib/efl.rb
@@ -4,7 +4,7 @@
 #
 module Efl
     #
-    VERSION = '0.0.5'
+    VERSION = '0.0.6'
     #
     autoload :EcoreEvas,    './lib/efl/ecore_evas.rb'
     autoload :EcoreGetopt,  './lib/efl/ecore_getopt.rb'
@@ -12,8 +12,9 @@ module Efl
     autoload :Ecore,        './lib/efl/ecore.rb'
     autoload :Edje,         './lib/efl/edje.rb'
     autoload :Eet,          './lib/efl/eet.rb'
-    autoload :EinHash,      './lib/efl/eina_hash.rb'
+    autoload :EinaHash,     './lib/efl/eina_hash.rb'
     autoload :EinaList,     './lib/efl/eina_list.rb'
+    autoload :EinaLog,      './lib/efl/eina_log.rb'
     autoload :Eina,         './lib/efl/eina.rb'
     autoload :Elm,          './lib/efl/elementary.rb'
     autoload :Evas,         './lib/efl/evas.rb'
