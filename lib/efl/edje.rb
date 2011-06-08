33190d0fc79d267cd6603e8305a5ba65e3fc2d79
Jérémy Zurcher <jeremy@asynk.ch>
2011-06-08 07:18:47 +0200

    fix ugly typo

--- lib/efl/edje.rb
+++ lib/efl/edje.rb
@@ -18,31 +18,31 @@ module Efl
             #
             def part_object_get part
                 # EAPI const Evas_Object *edje_object_part_object_get (const Evas_Object *obj, const char *part);
-                Evas::RevasObject.new Native.edje_object_part_object_get @ptr, part
+                Evas::REvasObject.new Native.edje_object_part_object_get @ptr, part
             end
             def part_swallow_get part
                 # EAPI Evas_Object *edje_object_part_swallow_get (const Evas_Object *obj, const char *part);
-                Evas::RevasObject.new Native.edje_object_part_swallow_get @ptr, part
+                Evas::REvasObject.new Native.edje_object_part_swallow_get @ptr, part
             end
             def external_object_get part
                 # EAPI Evas_Object *edje_object_part_external_object_get (const Evas_Object *obj, const char *part);
-                Evas::RevasObject.new Native.edje_object_part_external_object_get @ptr, part
+                Evas::REvasObject.new Native.edje_object_part_external_object_get @ptr, part
             end
             def external_content_get part, content
                 # EAPI Evas_Object *edje_object_part_external_content_get (const Evas_Object *obj, const char *part, const char *content);
-                Evas::RevasObject.new Native.edje_object_part_external_content_get @ptr, part, content
+                Evas::REvasObject.new Native.edje_object_part_external_content_get @ptr, part, content
             end
             def part_box_remove part, child
                 # EAPI Evas_Object *edje_object_part_box_remove (Evas_Object *obj, const char *part, Evas_Object *child);
-                Evas::RevasObject.new Native.edje_object_part_box_remove @ptr, part, child
+                Evas::REvasObject.new Native.edje_object_part_box_remove @ptr, part, child
             end
             def part_box_remove_at part, pos
                 # EAPI Evas_Object *edje_object_part_box_remove_at (Evas_Object *obj, const char *part, unsigned int pos);
-                Evas::RevasObject.new Native.edje_object_part_box_remove_at @ptr, part, pos
+                Evas::REvasObject.new Native.edje_object_part_box_remove_at @ptr, part, pos
             end
             def part_table_child_get part, col, row
                 # EAPI Evas_Object *edje_object_part_table_child_get (Evas_Object *obj, const char *part, unsigned int col, unsigned int row);
-                Evas::RevasObject.new Native.edje_object_part_table_child_get @ptr, part, col, row
+                Evas::REvasObject.new Native.edje_object_part_table_child_get @ptr, part, col, row
             end
             def file_get
                 f = FFI::MemoryPointer.new :pointer
