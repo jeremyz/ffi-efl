1e74a977c06ef999a3435a226da237f04baaacda
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-24 17:43:01 +0100

    regenerate native/eina_xattr

--- lib/efl/native/eina_xattr.rb
+++ lib/efl/native/eina_xattr.rb
@@ -45,19 +45,19 @@ module Efl
         # EAPI void *eina_xattr_get(const char *file, const char *attribute, ssize_t *size);
         [ :eina_xattr_get, [ :string, :string, :pointer ], :pointer ],
         # EAPI Eina_Bool eina_xattr_set(const char *file, const char *attribute, const void *data, ssize_t length, Eina_Xattr_Flags flags);
-        [ :eina_xattr_set, [ :string, :string, :pointer, :long, :eina_xattr_flags ], :eina_bool ],
+        [ :eina_xattr_set, [ :string, :string, :pointer, :long, :eina_xattr_flags ], :bool ],
         # EAPI Eina_Bool eina_xattr_string_set(const char *file, const char *attribute, const char *data, Eina_Xattr_Flags flags);
-        [ :eina_xattr_string_set, [ :string, :string, :string, :eina_xattr_flags ], :eina_bool ],
+        [ :eina_xattr_string_set, [ :string, :string, :string, :eina_xattr_flags ], :bool ],
         # EAPI char *eina_xattr_string_get(const char *file, const char *attribute);
         [ :eina_xattr_string_get, [ :string, :string ], :string ],
         # EAPI Eina_Bool eina_xattr_double_set(const char *file, const char *attribute, double value, Eina_Xattr_Flags flags);
-        [ :eina_xattr_double_set, [ :string, :string, :double, :eina_xattr_flags ], :eina_bool ],
+        [ :eina_xattr_double_set, [ :string, :string, :double, :eina_xattr_flags ], :bool ],
         # EAPI Eina_Bool eina_xattr_double_get(const char *file, const char *attribute, double *value);
-        [ :eina_xattr_double_get, [ :string, :string, :pointer ], :eina_bool ],
+        [ :eina_xattr_double_get, [ :string, :string, :pointer ], :bool ],
         # EAPI Eina_Bool eina_xattr_int_set(const char *file, const char *attribute, int value, Eina_Xattr_Flags flags);
-        [ :eina_xattr_int_set, [ :string, :string, :int, :eina_xattr_flags ], :eina_bool ],
+        [ :eina_xattr_int_set, [ :string, :string, :int, :eina_xattr_flags ], :bool ],
         # EAPI Eina_Bool eina_xattr_int_get(const char *file, const char *attribute, int *value);
-        [ :eina_xattr_int_get, [ :string, :string, :pointer ], :eina_bool ],
+        [ :eina_xattr_int_get, [ :string, :string, :pointer ], :bool ],
         ]
         #
         attach_fcts fcts
