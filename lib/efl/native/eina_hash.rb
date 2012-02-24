886921d6ec1301324aae77d7349bd813e49250b9
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-24 17:42:34 +0100

    regenerate native/eina_hash

--- lib/efl/native/eina_hash.rb
+++ lib/efl/native/eina_hash.rb
@@ -36,74 +36,74 @@ module Efl
         # typedef int (*Eina_Key_Hash) (const void *key, int key_length);
         callback :eina_key_hash_cb, [ :pointer, :int ], :int
         # typedef Eina_Bool (*Eina_Hash_Foreach) (const Eina_Hash *hash, const void *key, void *data, void *fdata);
-        callback :eina_hash_foreach_cb, [ :pointer, :pointer, :pointer, :pointer ], :eina_bool
+        callback :eina_hash_foreach_cb, [ :eina_hash, :pointer, :pointer, :pointer ], :bool
         #
         # VARIABLES
         #
         # FUNCTIONS
         fcts = [
         # EAPI Eina_Hash *eina_hash_new(Eina_Key_Length key_length_cb, Eina_Key_Cmp key_cmp_cb, Eina_Key_Hash key_hash_cb, Eina_Free_Cb data_free_cb, int buckets_power_size);
-        [ :eina_hash_new, [ :eina_key_length_cb, :eina_key_cmp_cb, :eina_key_hash_cb, :eina_free_cb, :int ], :pointer ],
+        [ :eina_hash_new, [ :eina_key_length_cb, :eina_key_cmp_cb, :eina_key_hash_cb, :eina_free_cb, :int ], :eina_hash ],
         # EAPI void eina_hash_free_cb_set(Eina_Hash *hash, Eina_Free_Cb data_free_cb);
-        [ :eina_hash_free_cb_set, [ :pointer, :eina_free_cb ], :void ],
+        [ :eina_hash_free_cb_set, [ :eina_hash, :eina_free_cb ], :void ],
         # EAPI Eina_Hash *eina_hash_string_djb2_new(Eina_Free_Cb data_free_cb);
-        [ :eina_hash_string_djb2_new, [ :eina_free_cb ], :pointer ],
+        [ :eina_hash_string_djb2_new, [ :eina_free_cb ], :eina_hash ],
         # EAPI Eina_Hash *eina_hash_string_superfast_new(Eina_Free_Cb data_free_cb);
-        [ :eina_hash_string_superfast_new, [ :eina_free_cb ], :pointer ],
+        [ :eina_hash_string_superfast_new, [ :eina_free_cb ], :eina_hash ],
         # EAPI Eina_Hash *eina_hash_string_small_new(Eina_Free_Cb data_free_cb);
-        [ :eina_hash_string_small_new, [ :eina_free_cb ], :pointer ],
+        [ :eina_hash_string_small_new, [ :eina_free_cb ], :eina_hash ],
         # EAPI Eina_Hash *eina_hash_int32_new(Eina_Free_Cb data_free_cb);
-        [ :eina_hash_int32_new, [ :eina_free_cb ], :pointer ],
+        [ :eina_hash_int32_new, [ :eina_free_cb ], :eina_hash ],
         # EAPI Eina_Hash *eina_hash_int64_new(Eina_Free_Cb data_free_cb);
-        [ :eina_hash_int64_new, [ :eina_free_cb ], :pointer ],
+        [ :eina_hash_int64_new, [ :eina_free_cb ], :eina_hash ],
         # EAPI Eina_Hash *eina_hash_pointer_new(Eina_Free_Cb data_free_cb);
-        [ :eina_hash_pointer_new, [ :eina_free_cb ], :pointer ],
+        [ :eina_hash_pointer_new, [ :eina_free_cb ], :eina_hash ],
         # EAPI Eina_Hash *eina_hash_stringshared_new(Eina_Free_Cb data_free_cb);
-        [ :eina_hash_stringshared_new, [ :eina_free_cb ], :pointer ],
+        [ :eina_hash_stringshared_new, [ :eina_free_cb ], :eina_hash ],
         # EAPI Eina_Bool eina_hash_add(Eina_Hash *hash, const void *key, const void *data);
-        [ :eina_hash_add, [ :pointer, :pointer, :pointer ], :eina_bool ],
+        [ :eina_hash_add, [ :eina_hash, :pointer, :pointer ], :bool ],
         # EAPI Eina_Bool eina_hash_direct_add(Eina_Hash *hash, const void *key, const void *data);
-        [ :eina_hash_direct_add, [ :pointer, :pointer, :pointer ], :eina_bool ],
+        [ :eina_hash_direct_add, [ :eina_hash, :pointer, :pointer ], :bool ],
         # EAPI Eina_Bool eina_hash_del(Eina_Hash *hash, const void *key, const void *data);
-        [ :eina_hash_del, [ :pointer, :pointer, :pointer ], :eina_bool ],
+        [ :eina_hash_del, [ :eina_hash, :pointer, :pointer ], :bool ],
         # EAPI void *eina_hash_find(const Eina_Hash *hash, const void *key);
-        [ :eina_hash_find, [ :pointer, :pointer ], :pointer ],
+        [ :eina_hash_find, [ :eina_hash, :pointer ], :pointer ],
         # EAPI void *eina_hash_modify(Eina_Hash *hash, const void *key, const void *data);
-        [ :eina_hash_modify, [ :pointer, :pointer, :pointer ], :pointer ],
+        [ :eina_hash_modify, [ :eina_hash, :pointer, :pointer ], :pointer ],
         # EAPI void *eina_hash_set(Eina_Hash *hash, const void *key, const void *data);
-        [ :eina_hash_set, [ :pointer, :pointer, :pointer ], :pointer ],
+        [ :eina_hash_set, [ :eina_hash, :pointer, :pointer ], :pointer ],
         # EAPI Eina_Bool eina_hash_move(Eina_Hash *hash, const void *old_key, const void *new_key);
-        [ :eina_hash_move, [ :pointer, :pointer, :pointer ], :eina_bool ],
+        [ :eina_hash_move, [ :eina_hash, :pointer, :pointer ], :bool ],
         # EAPI void eina_hash_free(Eina_Hash *hash);
-        [ :eina_hash_free, [ :pointer ], :void ],
+        [ :eina_hash_free, [ :eina_hash ], :void ],
         # EAPI void eina_hash_free_buckets(Eina_Hash *hash);
-        [ :eina_hash_free_buckets, [ :pointer ], :void ],
+        [ :eina_hash_free_buckets, [ :eina_hash ], :void ],
         # EAPI int eina_hash_population(const Eina_Hash *hash);
-        [ :eina_hash_population, [ :pointer ], :int ],
+        [ :eina_hash_population, [ :eina_hash ], :int ],
         # EAPI Eina_Bool eina_hash_add_by_hash(Eina_Hash *hash, const void *key, int key_length, int key_hash, const void *data);
-        [ :eina_hash_add_by_hash, [ :pointer, :pointer, :int, :int, :pointer ], :eina_bool ],
+        [ :eina_hash_add_by_hash, [ :eina_hash, :pointer, :int, :int, :pointer ], :bool ],
         # EAPI Eina_Bool eina_hash_direct_add_by_hash(Eina_Hash *hash, const void *key, int key_length, int key_hash, const void *data);
-        [ :eina_hash_direct_add_by_hash, [ :pointer, :pointer, :int, :int, :pointer ], :eina_bool ],
+        [ :eina_hash_direct_add_by_hash, [ :eina_hash, :pointer, :int, :int, :pointer ], :bool ],
         # EAPI Eina_Bool eina_hash_del_by_key_hash(Eina_Hash *hash, const void *key, int key_length, int key_hash);
-        [ :eina_hash_del_by_key_hash, [ :pointer, :pointer, :int, :int ], :eina_bool ],
+        [ :eina_hash_del_by_key_hash, [ :eina_hash, :pointer, :int, :int ], :bool ],
         # EAPI Eina_Bool eina_hash_del_by_key(Eina_Hash *hash, const void *key);
-        [ :eina_hash_del_by_key, [ :pointer, :pointer ], :eina_bool ],
+        [ :eina_hash_del_by_key, [ :eina_hash, :pointer ], :bool ],
         # EAPI Eina_Bool eina_hash_del_by_data(Eina_Hash *hash, const void *data);
-        [ :eina_hash_del_by_data, [ :pointer, :pointer ], :eina_bool ],
+        [ :eina_hash_del_by_data, [ :eina_hash, :pointer ], :bool ],
         # EAPI Eina_Bool eina_hash_del_by_hash(Eina_Hash *hash, const void *key, int key_length, int key_hash, const void *data);
-        [ :eina_hash_del_by_hash, [ :pointer, :pointer, :int, :int, :pointer ], :eina_bool ],
+        [ :eina_hash_del_by_hash, [ :eina_hash, :pointer, :int, :int, :pointer ], :bool ],
         # EAPI void *eina_hash_find_by_hash(const Eina_Hash *hash, const void *key, int key_length, int key_hash);
-        [ :eina_hash_find_by_hash, [ :pointer, :pointer, :int, :int ], :pointer ],
+        [ :eina_hash_find_by_hash, [ :eina_hash, :pointer, :int, :int ], :pointer ],
         # EAPI void *eina_hash_modify_by_hash(Eina_Hash *hash, const void *key, int key_length, int key_hash, const void *data);
-        [ :eina_hash_modify_by_hash, [ :pointer, :pointer, :int, :int, :pointer ], :pointer ],
+        [ :eina_hash_modify_by_hash, [ :eina_hash, :pointer, :int, :int, :pointer ], :pointer ],
         # EAPI Eina_Iterator *eina_hash_iterator_key_new(const Eina_Hash *hash);
-        [ :eina_hash_iterator_key_new, [ :pointer ], :pointer ],
+        [ :eina_hash_iterator_key_new, [ :eina_hash ], :pointer ],
         # EAPI Eina_Iterator *eina_hash_iterator_data_new(const Eina_Hash *hash);
-        [ :eina_hash_iterator_data_new, [ :pointer ], :pointer ],
+        [ :eina_hash_iterator_data_new, [ :eina_hash ], :pointer ],
         # EAPI Eina_Iterator *eina_hash_iterator_tuple_new(const Eina_Hash *hash);
-        [ :eina_hash_iterator_tuple_new, [ :pointer ], :pointer ],
+        [ :eina_hash_iterator_tuple_new, [ :eina_hash ], :pointer ],
         # EAPI void eina_hash_foreach(const Eina_Hash *hash, Eina_Hash_Foreach func, const void *fdata);
-        [ :eina_hash_foreach, [ :pointer, :eina_hash_foreach_cb, :pointer ], :void ],
+        [ :eina_hash_foreach, [ :eina_hash, :eina_hash_foreach_cb, :pointer ], :void ],
         # EAPI int eina_hash_superfast(const char *key, int len);
         [ :eina_hash_superfast, [ :string, :int ], :int ],
         ]
