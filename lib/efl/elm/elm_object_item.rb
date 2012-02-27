5eb017c40d7d41c1be5964052572cce3cbd9ae66
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-27 14:37:26 +0100

    add lib/efl/elm/elm_object_item.rb

---
+++ lib/efl/elm/elm_object_item.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/elm/elm_object'
+require 'efl/native/elm/elm_object_item'
+#
+module Efl
+    #
+    module Elm
+        #
+        class ElmObjectItem < ElmObject
+            #
+            search_prefixes 'elm_object_item_'
+            #
+        end
+        #
+    end
+end
+#
+# EOF
