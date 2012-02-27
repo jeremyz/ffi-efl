e46846c5740fd1af3c3d06e1991543c6e6a5c58c
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-27 14:53:57 +0100

    complete elementary_all.rb

--- lib/efl/elementary_all.rb
+++ lib/efl/elementary_all.rb
@@ -4,9 +4,17 @@
 require 'efl/elementary'
 #
 require 'efl/elm/elm_object'
+require 'efl/elm/elm_object_item'
 #
 require 'efl/elm/elm_bg'
+require 'efl/elm/elm_box'
+require 'efl/elm/elm_button'
+require 'efl/elm/elm_entry'
+require 'efl/elm/elm_icon'
 require 'efl/elm/elm_label'
+require 'efl/elm/elm_list'
+require 'efl/elm/elm_scroller'
+require 'efl/elm/elm_tooltip'
 require 'efl/elm/elm_win'
 #
 # EOF
