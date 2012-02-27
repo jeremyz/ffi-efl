e2e79d4b6ca8bfd031bf764ee45c44a68ae6287b
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-27 14:40:47 +0100

    add lib/efl/elm/elm_tooltip.rb

---
+++ lib/efl/elm/elm_tooltip.rb
@@ -0,0 +1,20 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/elm/elm_object'
+require 'efl/native/elm/elm_tooltip'
+#
+module Efl
+    #
+    module Elm
+        #
+        class ElmTooltip < ElmObject
+            #
+            search_prefixes 'elm_tooltip_', 'elm_object_tooltip_'
+            #
+        end
+        #
+    end
+end
+#
+# EOF
