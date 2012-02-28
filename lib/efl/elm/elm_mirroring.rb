7d35dcd880d9c29c40b853c88a44cf0cd685d16e
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-28 12:25:50 +0100

    parse elm_mirroring.h, generate elm_mirroring.rb

---
+++ lib/efl/elm/elm_mirroring.rb
@@ -0,0 +1,36 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/elm/elm_object'
+require 'efl/native/elm/elm_mirroring'
+#
+module Efl
+    #
+    module Elm
+        #
+        class ElmMirroring
+            #
+            def mirrored_get
+                Native::elm_object_mirrored_get @ptr
+            end
+            alias :mirrored :mirrored_get
+            #
+            def mirrored_set mirrored
+                Native::elm_object_mirrored_set @ptr, mirrored
+            end
+            #
+            def mirrored_automatic_get
+                Native::elm_object_mirrored_automatic_get @ptr
+            end
+            alias :mirrored_automatic :mirrored_automatic_get
+            #
+            def mirrored_automatic_set auto
+                Native::elm_object_mirrored_automatic_set @ptr, auto
+            end
+            #
+        end
+        #
+    end
+end
+#
+# EOF
---
+++ lib/efl/native/elm/elm_mirroring.rb
@@ -0,0 +1,45 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/native'
+require 'efl/native/elementary'
+#
+module Efl
+    #
+    module ElmMirroring
+        #
+        FCT_PREFIX = 'elm_mirroring_' unless const_defined? :FCT_PREFIX
+        #
+        def self.method_missing meth, *args, &block
+            sym = Efl::MethodResolver.resolve self, meth, FCT_PREFIX
+            self.send sym, *args, &block
+        end
+        #
+    end
+    #
+    module Native
+        #
+        ffi_lib 'elementary-ver-pre-svn-09.so.0'
+        #
+        # FUNCTIONS
+        fcts = [
+        # EAPI Eina_Bool elm_mirrored_get(void);
+        [ :elm_mirrored_get, [  ], :bool ],
+        # EAPI void elm_mirrored_set(Eina_Bool mirrored);
+        [ :elm_mirrored_set, [ :bool ], :void ],
+        # EAPI Eina_Bool elm_object_mirrored_get(const Evas_Object *obj);
+        [ :elm_object_mirrored_get, [ :evas_object ], :bool ],
+        # EAPI void elm_object_mirrored_set(Evas_Object *obj, Eina_Bool mirrored);
+        [ :elm_object_mirrored_set, [ :evas_object, :bool ], :void ],
+        # EAPI Eina_Bool elm_object_mirrored_automatic_get(const Evas_Object *obj);
+        [ :elm_object_mirrored_automatic_get, [ :evas_object ], :bool ],
+        # EAPI void elm_object_mirrored_automatic_set(Evas_Object *obj, Eina_Bool automatic);
+        [ :elm_object_mirrored_automatic_set, [ :evas_object, :bool ], :void ],
+        ]
+        #
+        attach_fcts fcts
+        #
+    end
+end
+#
+# EOF
