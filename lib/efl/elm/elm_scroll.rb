e0929df4e94fb8d4b3216f52694b558b3f04739f
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-28 11:45:25 +0100

    parse elm_scroll.h, generate elm_scroll.rb

---
+++ lib/efl/elm/elm_scroll.rb
@@ -0,0 +1,50 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/elm/elm_object'
+require 'efl/native/elm/elm_scroll'
+#
+module Efl
+    #
+    module Elm
+        #
+        class ElmObject
+            #
+            def scroll_hold_push
+                Native::elm_object_scroll_hold_push @ptr
+            end
+            #
+            def scroll_hold_pop
+                Native::elm_object_scroll_hold_pop @ptr
+            end
+            #
+            def scroll_freeze_push
+                Native::elm_object_scroll_freeze_push @ptr
+            end
+            #
+            def scroll_freeze_pop
+                Native::elm_object_scroll_freeze_pop @ptr
+            end
+            #
+            def scroll_lock_x_set lock
+                Native::elm_object_scroll_lock_x_set @ptr, lock
+            end
+            #
+            def scroll_lock_y_set lock
+                Native::elm_object_scroll_lock_x_set @ptr, lock
+            end
+            #
+            def scroll_lock_x_get
+                Native::elm_object_scroll_lock_x_set @ptr
+            end
+            #
+            def scroll_lock_y_get
+                Native::elm_object_scroll_lock_x_set @ptr
+            end
+            #
+        end
+        #
+    end
+end
+#
+# EOF
---
+++ lib/efl/native/elm/elm_scroll.rb
@@ -0,0 +1,49 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/native'
+require 'efl/native/elementary'
+#
+module Efl
+    #
+    module ElmScroll
+        #
+        FCT_PREFIX = 'elm_scroll_' unless const_defined? :FCT_PREFIX
+        #
+        def self.method_missing meth, *args, &block
+            sym = Efl::MethodResolver.resolve self, meth, FCT_PREFIX
+            self.send sym, *args, &block
+        end
+        #
+    end
+    #
+    module Native
+        #
+        ffi_lib 'elementary-ver-pre-svn-09.so.0'
+        #
+        # FUNCTIONS
+        fcts = [
+        # EAPI void elm_object_scroll_hold_push(Evas_Object *obj);
+        [ :elm_object_scroll_hold_push, [ :evas_object ], :void ],
+        # EAPI void elm_object_scroll_hold_pop(Evas_Object *obj);
+        [ :elm_object_scroll_hold_pop, [ :evas_object ], :void ],
+        # EAPI void elm_object_scroll_freeze_push(Evas_Object *obj);
+        [ :elm_object_scroll_freeze_push, [ :evas_object ], :void ],
+        # EAPI void elm_object_scroll_freeze_pop(Evas_Object *obj);
+        [ :elm_object_scroll_freeze_pop, [ :evas_object ], :void ],
+        # EAPI void elm_object_scroll_lock_x_set(Evas_Object *obj, Eina_Bool lock);
+        [ :elm_object_scroll_lock_x_set, [ :evas_object, :bool ], :void ],
+        # EAPI void elm_object_scroll_lock_y_set(Evas_Object *obj, Eina_Bool lock);
+        [ :elm_object_scroll_lock_y_set, [ :evas_object, :bool ], :void ],
+        # EAPI Eina_Bool elm_object_scroll_lock_x_get(const Evas_Object *obj);
+        [ :elm_object_scroll_lock_x_get, [ :evas_object ], :bool ],
+        # EAPI Eina_Bool elm_object_scroll_lock_y_get(const Evas_Object *obj);
+        [ :elm_object_scroll_lock_y_get, [ :evas_object ], :bool ],
+        ]
+        #
+        attach_fcts fcts
+        #
+    end
+end
+#
+# EOF
