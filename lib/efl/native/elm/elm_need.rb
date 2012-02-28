e2fa9dcf9bf6b3ef5428f1cc13feacb2cfd0e6a9
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-28 12:27:49 +0100

    parse elm_need.h, generate elm_need.rb

---
+++ lib/efl/native/elm/elm_need.rb
@@ -0,0 +1,41 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/native'
+require 'efl/native/elementary'
+#
+module Efl
+    #
+    module ElmNeed
+        #
+        FCT_PREFIX = 'elm_need_' unless const_defined? :FCT_PREFIX
+        #
+        def self.method_missing meth, *args, &block
+            sym = Efl::MethodResolver.resolve self, meth, FCT_PREFIX
+            self.send sym, *args, &block
+        end
+        #
+    end
+    #
+    module Native
+        #
+        ffi_lib 'elementary-ver-pre-svn-09.so.0'
+        #
+        # FUNCTIONS
+        fcts = [
+        # EAPI Eina_Bool elm_need_efreet(void);
+        [ :elm_need_efreet, [  ], :bool ],
+        # EAPI Eina_Bool elm_need_e_dbus(void);
+        [ :elm_need_e_dbus, [  ], :bool ],
+        # EAPI Eina_Bool elm_need_ethumb(void);
+        [ :elm_need_ethumb, [  ], :bool ],
+        # EAPI Eina_Bool elm_need_web(void);
+        [ :elm_need_web, [  ], :bool ],
+        ]
+        #
+        attach_fcts fcts
+        #
+    end
+end
+#
+# EOF
