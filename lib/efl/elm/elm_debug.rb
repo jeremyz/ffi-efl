7ed7256e9716118f0e9d8bc728c006bcf1f3cbf8
Jérémy Zurcher <jeremy@asynk.ch>
2012-02-28 15:58:55 +0100

    parse elm_debug.h, generate elm_debug.rb

---
+++ lib/efl/elm/elm_debug.rb
@@ -0,0 +1,25 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/elm/elm_object'
+require 'efl/native/elm/elm_scroll'
+#
+module Efl
+    #
+    module Elm
+        #
+        class ElmObject
+            #
+            def tree_dump
+                Native::elm_object_tree_dump @ptr
+            end
+            def tree_dot_dump filepath
+                Native::elm_object_tree_dot_dump @ptr, filepath
+            end
+            #
+        end
+        #
+    end
+end
+#
+# EOF
---
+++ lib/efl/native/elm/elm_debug.rb
@@ -0,0 +1,37 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+#
+require 'efl/native'
+require 'efl/native/elementary'
+#
+module Efl
+    #
+    module ElmDebug
+        #
+        FCT_PREFIX = 'elm_debug_' unless const_defined? :FCT_PREFIX
+        #
+        def self.method_missing meth, *args, &block
+            sym = Efl::MethodResolver.resolve self, meth, FCT_PREFIX
+            self.send sym, *args, &block
+        end
+        #
+    end
+    #
+    module Native
+        #
+        ffi_lib 'elementary-ver-pre-svn-09.so.0'
+        #
+        # FUNCTIONS
+        fcts = [
+        # EAPI void elm_object_tree_dump(const Evas_Object *top);
+        [ :elm_object_tree_dump, [ :evas_object ], :void ],
+        # EAPI void elm_object_tree_dot_dump(const Evas_Object *top, const char *file);
+        [ :elm_object_tree_dot_dump, [ :evas_object, :string ], :void ],
+        ]
+        #
+        attach_fcts fcts
+        #
+    end
+end
+#
+# EOF
